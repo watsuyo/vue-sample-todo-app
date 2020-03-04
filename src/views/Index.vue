2a9af94829394fe0ee30e21cf33e1a83129bc76a
Kenzo Ogawa <mazaatv.kz@gmail.com>
2020-03-04 17:22:27 +0900

    add new todo

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -1,6 +1,8 @@
 <template>
   <div>
     <h1>Todoリスト</h1>
+    <input type="text" v-model="newTitle" placeholder="ToDoを入力" />
+    <button v-on:click="addTodo">追加</button>
     <TodoItemList class="todo-item-list" :todoList="todoList" />
   </div>
 </template>
@@ -12,9 +14,26 @@ export default {
   components: {
     TodoItemList
   },
-
+  methods: {
+    addTodo: function() {
+      this.todoList.push({
+        uid: this.getUniqueStr(),
+        title: this.newTitle,
+        isDone: false,
+        createdAt: new Date()
+      })
+      this.newTitle = ''
+    },
+    getUniqueStr: function() {
+      return (
+        new Date().getTime().toString(16) +
+        Math.floor(1000 * Math.random()).toString(16)
+      )
+    }
+  },
   data() {
     return {
+      newTitle: '',
       todoList: [
         {
           uid: 'bp0m4jl4a08',
