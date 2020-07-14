52819ce04b19ab52d564d22b2a9f8c1abf48ae8c
h_008 <h_008@mac.com>
2020-07-14 21:10:52 +0900

    todoItem追加機能を実装

---
+++ src/components/TodoItemForm.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <input type="text" v-model="newTodoItem" />
+    <button @click="addTodoItem">Add</button>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  data() {
+    return {
+      newTodoItem: ''
+    }
+  },
+  methods: {
+    addTodoItem() {
+      this.$emit('addtodoitem', this.newTodoItem)
+      this.newTodoItem = ''
+    }
+  }
+})
+</script>
